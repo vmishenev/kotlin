586d43896e873b84587f638b1d4a2ea9c7d7ea76
vmishenev <vad-mishenev@yandex.ru>
2022-12-16 01:42:41 +0200

    Update Dokka to 1.7.20-dev-187

--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
+++ libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
@@ -7,6 +7,7 @@ import org.jetbrains.dokka.plugability.DokkaPlugin
 class SamplesTransformerPlugin : DokkaPlugin() {
     private val dokkaBase by lazy { plugin<DokkaBase>() }
 
+    @Suppress("unused")
     val kotlinWebsiteSamplesTransformer by extending {
         CoreExtensions.pageTransformer providing ::KotlinWebsiteSamplesTransformer override dokkaBase.defaultSamplesTransformer order {
             before(dokkaBase.pageMerger)
